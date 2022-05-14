ee99d6240f4c105aadab3ea19d130f2ef2d75a57
DarkStarSonic <darkstart020543@gmail.com>
2022-05-15 02:42:13 +0600

    mini changes

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -2,6 +2,9 @@
   <div>
     <nav-bar/>
   </div>
+  <div>
+    <b-table striped hover :items="schedule" :fields="fields"></b-table>
+  </div>
 </template>
 <script>
 import NavBar from "@/components/NavBar/NavBar";
@@ -9,7 +12,22 @@ import NavBar from "@/components/NavBar/NavBar";
 export default {
   components: {
     NavBar
+  }, data() {
+    return {
+      fields: [],
+      schedule: []
+    }
   },
+  mounted() {
+  },
+  methods: {
+    getLesson() {
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+      }
+    }
+  }
 }
 </script>
 <style>
